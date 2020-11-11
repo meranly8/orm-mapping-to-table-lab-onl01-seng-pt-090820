bb3c4d746588aeae2ee3cf1763bee0c6b23fbdaa
Michelle Ranly <meranly8@gmail.com>
2020-11-11 06:43:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,4 +8,14 @@ class Student
     @id = id
   end
 
+  def self.create_table
+    sql = <<- SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+      )
+    SQL
+  end
+
 end
